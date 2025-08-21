c54db5b31a9b5a2c9a792ecc9e379694c5c6f08b
Akash2Patil <akashpatil1321999@gmail.com>
2025-08-21 23:08:13 +0530

    structure of app.jsx updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import Projects from './page/Projects'
 import Home from './page/Home'
+import Routing from './utils/Routing'
+import Navbar from './component/Navbar'
 
 const App = () => {
   return (
     <>
-  <Home/>
-    
-    <Projects/>
+    <div className='w-full bg-black flex justify-center'>
+    <div className='w-full max-w-6xl'>
+    <Navbar/>
+    <Routing/>
+    </div>
+    </div>
     </>
   )
 }
