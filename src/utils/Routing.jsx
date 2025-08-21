b864b1be1f449ad8144044de0ff1be7c637833ba
Akash2Patil <akashpatil1321999@gmail.com>
2025-08-21 23:11:13 +0530

    routing updated in project

--- src/utils/Routing.jsx
+++ src/utils/Routing.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Projects from '../page/Projects'
-import Hero from '../page/Home'
+import Home from '../page/Home'
 
 const Routing = () => {
   return (
     <Routes>
-        <Route path="/" element={<Hero/>} />
+        <Route path="/" element={<Home/>} />
         <Route path="/projects" element={<Projects/>} />
     </Routes>
   )
